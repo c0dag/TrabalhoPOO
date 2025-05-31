1007a422bf0564ed2254ec7078c28c8cd1af021e
unknown <codapsilva123@gmail.com>
2025-05-31 14:55:13 -0300

    reestruturacao

---
+++ src/br/com/empresa/ecommerce/Produto/Produto.java
@@ -0,0 +1,13 @@
+package br.com.empresa.ecommerce.Produto;
+
+public class Produto {
+    private String nome;
+    private int preco;
+
+
+    public Produto(String nome, int preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+}
